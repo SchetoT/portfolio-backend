8ba771139ee3eac0f200295615dfbaf598ac459d
SchetoT <schetot@gmail.com>
2025-01-31 17:06:47 -0300

    solucion de problemas en el log

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';  
-import connectToDB from './connect.js';
+import connectToDB from './src/utils/connect.js';
 import projectRoutes from '../routes/projectsRoutes.js';
 
 const app = express();
