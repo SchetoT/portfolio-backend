b2ed7813fb4f02b23d661b047abdf1d8c4604e14
SchetoT <schetot@gmail.com>
2025-01-31 17:03:42 -0300

    arreglos

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-import app from './src/utils/app.js'; 
-import seedDatabase from './seed.js';
-const PORT = 3000;
+import express from 'express';
+import cors from 'cors';  
+import connectToDB from './connect.js';
+import projectRoutes from '../routes/projectsRoutes.js';
 
+const app = express();
 
-const allowedOrigins = ['https://portfolio-frontend-3sknlz8jx-tomas-projects-e5b3bb3f.vercel.app'];
+connectToDB();
+
+app.use(express.json());
+
+const allowedOrigins = ['https://portfolio-frontend-3sknlz8jx-tomas-projects-e5b3bb3f.vercel.app', 'https://portfolio-frontend-nine-alpha.vercel.app']; // Asegúrate de incluir tu URL de frontend en Vercel
 app.use(cors({
 origin: allowedOrigins,
 methods: ['GET', 'POST', 'PUT', 'DELETE'],
 allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-app.use((req, res, next) => {
-next();
+
+app.get('/', (req, res) => {
+res.send('Servidor funcionando correctamente');
 });
 
-app.listen(PORT, () => {
-console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
 
-seedDatabase();
+app.use("/api/projects", projectRoutes);
+
+export default app;
