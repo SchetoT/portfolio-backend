ec1a8ed9b9cca24c20dd448e45524361cc68a04f
SchetoT <schetot@gmail.com>
2025-01-31 16:53:17 -0300

    solucion de conexion del front con el back

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-import app from './src/utils/app.js';
+import express from 'express';
+import cors from 'cors';
 import seedDatabase from './seed.js';
+import app from './src/utils/app.js'; 
 const PORT = 3000;
+
+const app = express();
+
+const allowedOrigins = ['https://tu-frontend.vercel.app']; // Cambia a tu URL de Vercel
+app.use(cors({
+origin: allowedOrigins, 
+methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+allowedHeaders: ['Content-Type', 'Authorization'], 
+}));
+
 
 app.use((req, res, next) => {
 next();
 });
 
+
 app.listen(PORT, () => {
 console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
-seedDatabase(); 
+
+seedDatabase();
